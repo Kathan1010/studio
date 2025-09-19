5a89d9c7ca1426a876c6634707b407f45d425c54
Mr. Techie <kathangajera@gmail.com>
2025-09-19 03:13:19 +0000

    An exercise in web-based 3D. in footer in place of this text you write b

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
           <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
             <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
               <p className="text-center text-sm leading-loose md:text-left text-muted-foreground">
-                Built for CloneFest 2025. An exercise in web-based 3D.
+                Built by team Igniv0x
               </p>
             </div>
           </div>
