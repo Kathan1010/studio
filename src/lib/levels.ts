0806d5c52985c8173187226d98ed087719d75c3c
Mr. Techie <kathangajera@gmail.com>
2025-09-20 04:04:54 +0000

    still the same

--- src/lib/levels.ts
+++ src/lib/levels.ts
@@ -181,7 +181,7 @@ export const levels: Level[] = [
     id: 6,
     name: "The Labyrinth",
     par: 5,
-    startPosition: [-12, 0.2, 10],
+    startPosition: [-24, 0.2, 24],
     holePosition: [10, 0.01, -10],
     holeRadius: 0.3,
     obstacles: [
